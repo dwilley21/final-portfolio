5dd6572328a56d48f6543056cb607b2ca60e69f7
kevinmontanez <kmontanez@multiservice.com>
2018-06-11 18:26:02 -0500

    fixed your mess

--- src/app/material-custom/material-custom.module.ts
+++ src/app/material-custom/material-custom.module.ts
@@ -1,33 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatAutocompleteModule } from '@angular/material';
-import { MatButtonModule } from '@angular/material';
-import { MatButtonToggleModule } from '@angular/material';
-import { MatCardModule } from '@angular/material';
-import {MatCheckboxModule} from '@angular/material';
-import {MatChipsModule} from '@angular/material';
-import {MatDatepickerModule} from '@angular/material';
-import {MatDialogModule} from '@angular/material';
-import {MatExpansionModule} from '@angular/material';
-import {MatFormFieldModule} from '@angular/material';
-import {MatIconModule} from '@angular/material';
-import {MatInputModule} from '@angular/material';
-import {MatListModule} from '@angular/material';
-import {MatMenuModule} from '@angular/material';
-import {MatNativeDateModule} from '@angular/material';
-import {MatPaginatorModule} from '@angular/material';
-import {MatProgressBarModule} from '@angular/material';
-import {MatRippleModule} from '@angular/material';
-import {MatSelectModule} from '@angular/material';
-import {MatSidenavModule} from '@angular/material';
-import {MatSliderModule} from '@angular/material';
-import {MatSlideToggleModule} from '@angular/material';
-import {MatSortModule} from '@angular/material';
-import {MatTableModule} from '@angular/material';
-import {MatTabsModule} from '@angular/material';
-import {MatToolbarModule} from '@angular/material';
-import {MatTooltipModule} from '@angular/material';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatSidenavModule, } from '@angular/material';
 
 @NgModule({
   imports: [
@@ -60,6 +61,7 @@ import {MatTooltipModule} from '@angular/material';
     MatToolbarModule,
     MatTooltipModule,
     MatSidenavModule,
+    BrowserAnimationsModule
   ],
   exports: [
     MatAutocompleteModule,
@@ -89,6 +91,7 @@ import {MatTooltipModule} from '@angular/material';
     MatToolbarModule,
     MatTooltipModule,
     MatSidenavModule,
+    BrowserAnimationsModule
   ],
   declarations: []
 })
